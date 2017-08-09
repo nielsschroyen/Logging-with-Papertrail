9af47c27455da4d58e3712ddd0c2cb1cfd385232
Niels Schroyen <niels.schroyen@gmail.com>
2017-08-09 14:19:00 +0200

    Host and Port dynamic as config

--- php/papertrail-error-handler.php
+++ php/papertrail-error-handler.php
@@ -3,9 +3,15 @@
 require_once 'papertrail-sender.php';
 
 class Papertrail_ErrorHandler {    
-    public static function error_handler(  $errno , $errstr, $errfile, $errline, $errcontext) {
+
+
+    public static function error_handler(  $errno , $errstr, $errfile, $errline) {
+        $options = get_option( 'papertrail_options' );
+        $host =  esc_attr( $options['host']);
+        $port =  esc_attr( $options['port']);
+        
         $message = "[ERROR] " .  $errstr . ' ' .  $errfile . ' ' . $errline;
-        Papertrail_Sender::send_remote_syslog( $message, "WordPress", "Dev-Machine", 'URL FOR PAPERTRAIL', 'PORT FOR PAPERTRAIL');
+        Papertrail_Sender::send_remote_syslog( $message, "WordPresso", "Dev-Machine",$host, $port);
 	}
 
     public static function setup_handler(){
