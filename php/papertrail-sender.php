c3d17540c44bab9a675a4e689362b745abe4f294
Niels Schroyen <niels.schroyen@gmail.com>
2017-08-13 16:41:07 +0200

    #1 : Change sockets to streams, easier support for tcp https://stackoverflow.com/questions/9760548/php-sockets-vs-streams

--- php/papertrail-sender.php
+++ php/papertrail-sender.php
@@ -1,12 +1,15 @@
 <?php
-
 class Papertrail_Sender {
-	  public static function send_remote_syslog( $message, $system, $program, $papertrailUrl, $papertrailPort) {
-				$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-				foreach(explode("\n", $message) as $line) {
-						$syslog_message = "<22>" . date('M d H:i:s ') . $system . ' ' . $program . ': ' . $line;
-						socket_sendto($sock, $syslog_message, strlen($syslog_message), 0, $papertrailUrl, $papertrailPort);
-				}
-				socket_close($sock);
-    }
+	public static function send_remote_syslog( $message, $system, $program, $papertrailUrl, $papertrailPort) {
+		try {
+			$client = stream_socket_client("udp://$papertrailUrl:$papertrailPort",$errno, $errstr);			
+			foreach(explode("\n", $message) as $line) {
+					$syslog_message = "<22>" . date('M d H:i:s ') . $system . ' ' . $program . ': ' . $line;
+					fwrite($client, $syslog_message);			
+			}
+			fclose($client);	
+		} catch (Exception $e) {
+			//Ignore exception for now
+		}
+	}
 }
