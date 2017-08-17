1ab3fc8acfb626bfe2ee1fad01cb10f287d2c919
Niels Schroyen <niels.schroyen@gmail.com>
2017-08-17 23:30:56 +0200

    #5 Create release script

---
+++ create_release.sh
@@ -0,0 +1,8 @@
+[ -e papertrail-for-wordpress.zip ] && rm papertrail-for-wordpress.zip
+
+zip papertrail-for-wordpress.zip README.md
+zip papertrail-for-wordpress.zip readme.txt
+zip papertrail-for-wordpress.zip LICENSE
+zip papertrail-for-wordpress.zip papertrail-for-wordpress.php
+zip papertrail-for-wordpress.zip uninstall.php
+zip -r papertrail-for-wordpress.zip php
